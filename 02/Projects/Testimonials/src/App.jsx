68209ccc444bf465aae43b96216ba5300136c297
zaid-devel <184748306+zaid-devel@users.noreply.github.com>
2024-12-19 10:45:59 +0530

    Testimonials

---
+++ 02/Projects/Testimonials/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Testimonials from './Components/Testimonials'
+
+const App = () => {
+  return (
+    <div>
+      <Testimonials />
+    </div>
+  )
+}
+
+export default App
