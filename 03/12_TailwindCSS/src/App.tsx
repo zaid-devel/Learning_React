f6f59a8d35b29a8c20fd834696d471a78b9b3dda
zaid-devel <184748306+zaid-devel@users.noreply.github.com>
2025-01-02 19:01:49 +0530

    Tailwind CSS

---
+++ 03/12_TailwindCSS/src/App.tsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="lg:flex lg:gap-x-12">
+        <a href="#" className="text-sm/6 font-semibold text-gray-900">Product</a>
+        <a href="#" className="text-sm/6 font-semibold text-gray-900">Features</a>
+        <a href="#" className="text-sm/6 font-semibold text-gray-900">Marketplace</a>
+        <a href="#" className="text-sm/6 font-semibold text-gray-900">Company</a>
+    </div>
+  )
+};
+
+export default App;
