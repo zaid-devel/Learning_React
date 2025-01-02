c24313fbf5742c3e0cb8b46c042aa0c7a14fd786
zaid-devel <184748306+zaid-devel@users.noreply.github.com>
2025-01-02 21:47:06 +0530

    Daisy UI

---
+++ 03/14_DaisyUI/src/App.tsx
@@ -0,0 +1,36 @@
+
+
+const App = () => {
+  return (
+    <div>
+    <div>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
+    </div>
+
+    {/* Open the modal using document.getElementById('ID').showModal() method */}
+    <button className="btn" onClick={()=>(document.getElementById('my_modal_1' ) as HTMLDialogElement)?.showModal()}>open modal</button>
+    <dialog id="my_modal_1" className="modal">
+      <div className="modal-box">
+        <h3 className="font-bold text-lg">Hello!</h3>
+        <p className="py-4">Press ESC key or click the button below to close</p>
+        <div className="modal-action">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn">Close</button>
+          </form>
+        </div>
+      </div>
+    </dialog>
+
+    </div>
+    
+  )
+}
+
+export default App
